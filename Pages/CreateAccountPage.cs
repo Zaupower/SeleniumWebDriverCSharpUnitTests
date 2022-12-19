72878b4a9395dd402ddbaf0bdca8360340dcd088
Zaupower <56695737+Zaupower@users.noreply.github.com>
2022-12-19 11:44:15 +0000

    added Create user intput tests

--- Pages/CreateAccountPage.cs
+++ Pages/CreateAccountPage.cs
@@ -7,6 +7,22 @@ namespace sleeniumTest.Pages
     {
         [FindsBy(How = How.ClassName, Using = "base")]
         private IWebElement _createAccountTitle;
+
+        [FindsBy(How = How.Id, Using = "firstname")]
+        private IWebElement _firstNameInput;
+
+        [FindsBy(How = How.Id, Using = "lasttname")]
+        private IWebElement _lastNameInput;
+
+        [FindsBy(How = How.Id, Using = "email_address")]
+        private IWebElement _emailInput;
+
+        [FindsBy(How = How.Id, Using = "password")]
+        private IWebElement _passwordInput;
+
+        [FindsBy(How = How.Id, Using = "password-confirmation")]
+        private IWebElement _passwordConfirmationInput;
+
         public CreateAccountPage(IWebDriver driver) : base(driver)
         {
         }
